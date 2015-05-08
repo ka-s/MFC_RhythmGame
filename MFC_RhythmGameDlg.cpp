d7167db18b7efc2a514e591e5a67c7e2c1b10656
ka-s_mac <using0namespace0kas@gmail.com>
2015-05-08 16:36:24 +0900

    FixIndent

--- MFC_RhythmGameDlg.cpp
+++ MFC_RhythmGameDlg.cpp
@@ -36,10 +36,12 @@ namespace
 // --------------------------------
 void PlayMetronome()
 {
-    while (isMetronome) {
+    while (isMetronome)
+    {
         if (
-            (timeGetTime() - time_start) >= ( (DWORD)( ((60.0 / bpm) * 1000.0 ) * tempoCount) )
-            ) {
+            (timeGetTime() - time_start) >= ((DWORD)(((60.0 / bpm) * 1000.0) * tempoCount))
+            )
+        {
             // テンポカウントを+1
             tempoCount += 1.0;
             // 鳴らす
@@ -53,17 +55,17 @@ void PlayMetronome()
 class CAboutDlg : public CDialogEx
 {
 public:
-	CAboutDlg();
+    CAboutDlg();
 
-// ダイアログ データ
-	enum { IDD = IDD_ABOUTBOX };
+    // ダイアログ データ
+    enum { IDD = IDD_ABOUTBOX };
 
-	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV サポート
-
-// 実装
 protected:
-	DECLARE_MESSAGE_MAP()
+    virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV サポート
+
+    // 実装
+protected:
+    DECLARE_MESSAGE_MAP()
 };
 
 CAboutDlg::CAboutDlg() : CDialogEx(CAboutDlg::IDD)
@@ -72,7 +74,7 @@ CAboutDlg::CAboutDlg() : CDialogEx(CAboutDlg::IDD)
 
 void CAboutDlg::DoDataExchange(CDataExchange* pDX)
 {
-	CDialogEx::DoDataExchange(pDX);
+    CDialogEx::DoDataExchange(pDX);
 }
 
 BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
@@ -84,20 +86,20 @@ END_MESSAGE_MAP()
 
 
 CMFC_RhythmGameDlg::CMFC_RhythmGameDlg(CWnd* pParent /*=NULL*/)
-	: CDialogEx(CMFC_RhythmGameDlg::IDD, pParent)
+    : CDialogEx(CMFC_RhythmGameDlg::IDD, pParent)
 {
-	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+    m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
 }
 
 void CMFC_RhythmGameDlg::DoDataExchange(CDataExchange* pDX)
 {
-	CDialogEx::DoDataExchange(pDX);
+    CDialogEx::DoDataExchange(pDX);
 }
 
 BEGIN_MESSAGE_MAP(CMFC_RhythmGameDlg, CDialogEx)
-	ON_WM_SYSCOMMAND()
-	ON_WM_PAINT()
-	ON_WM_QUERYDRAGICON()
+    ON_WM_SYSCOMMAND()
+    ON_WM_PAINT()
+    ON_WM_QUERYDRAGICON()
     ON_BN_CLICKED(IDC_BUTTON1, &CMFC_RhythmGameDlg::OnBnClickedButton1)
     ON_BN_CLICKED(IDC_BUTTON2, &CMFC_RhythmGameDlg::OnBnClickedButton2)
     ON_BN_CLICKED(IDC_BUTTON3, &CMFC_RhythmGameDlg::OnBnClickedButton3)
@@ -108,49 +110,49 @@ END_MESSAGE_MAP()
 
 BOOL CMFC_RhythmGameDlg::OnInitDialog()
 {
-	CDialogEx::OnInitDialog();
+    CDialogEx::OnInitDialog();
 
-	// "バージョン情報..." メニューをシステム メニューに追加します。
+    // "バージョン情報..." メニューをシステム メニューに追加します。
 
-	// IDM_ABOUTBOX は、システム コマンドの範囲内になければなりません。
-	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
-	ASSERT(IDM_ABOUTBOX < 0xF000);
+    // IDM_ABOUTBOX は、システム コマンドの範囲内になければなりません。
+    ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+    ASSERT(IDM_ABOUTBOX < 0xF000);
 
-	CMenu* pSysMenu = GetSystemMenu(FALSE);
-	if (pSysMenu != NULL)
-	{
-		BOOL bNameValid;
-		CString strAboutMenu;
-		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
-		ASSERT(bNameValid);
-		if (!strAboutMenu.IsEmpty())
-		{
-			pSysMenu->AppendMenu(MF_SEPARATOR);
-			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
-		}
-	}
+    CMenu* pSysMenu = GetSystemMenu(FALSE);
+    if (pSysMenu != NULL)
+    {
+        BOOL bNameValid;
+        CString strAboutMenu;
+        bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+        ASSERT(bNameValid);
+        if (!strAboutMenu.IsEmpty())
+        {
+            pSysMenu->AppendMenu(MF_SEPARATOR);
+            pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+        }
+    }
 
-	// このダイアログのアイコンを設定します。アプリケーションのメイン ウィンドウがダイアログでない場合、
-	//  Framework は、この設定を自動的に行います。
-	SetIcon(m_hIcon, TRUE);			// 大きいアイコンの設定
-	SetIcon(m_hIcon, FALSE);		// 小さいアイコンの設定
+    // このダイアログのアイコンを設定します。アプリケーションのメイン ウィンドウがダイアログでない場合、
+    //  Framework は、この設定を自動的に行います。
+    SetIcon(m_hIcon, TRUE);			// 大きいアイコンの設定
+    SetIcon(m_hIcon, FALSE);		// 小さいアイコンの設定
 
-	// TODO: 初期化をここに追加します。
+    // TODO: 初期化をここに追加します。
 
-	return TRUE;  // フォーカスをコントロールに設定した場合を除き、TRUE を返します。
+    return TRUE;  // フォーカスをコントロールに設定した場合を除き、TRUE を返します。
 }
 
 void CMFC_RhythmGameDlg::OnSysCommand(UINT nID, LPARAM lParam)
 {
-	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
-	{
-		CAboutDlg dlgAbout;
-		dlgAbout.DoModal();
-	}
-	else
-	{
-		CDialogEx::OnSysCommand(nID, lParam);
-	}
+    if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+    {
+        CAboutDlg dlgAbout;
+        dlgAbout.DoModal();
+    }
+    else
+    {
+        CDialogEx::OnSysCommand(nID, lParam);
+    }
 }
 
 // ダイアログに最小化ボタンを追加する場合、アイコンを描画するための
@@ -159,34 +161,34 @@ void CMFC_RhythmGameDlg::OnSysCommand(UINT nID, LPARAM lParam)
 
 void CMFC_RhythmGameDlg::OnPaint()
 {
-	if (IsIconic())
-	{
-		CPaintDC dc(this); // 描画のデバイス コンテキスト
+    if (IsIconic())
+    {
+        CPaintDC dc(this); // 描画のデバイス コンテキスト
 
-		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+        SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
 
-		// クライアントの四角形領域内の中央
-		int cxIcon = GetSystemMetrics(SM_CXICON);
-		int cyIcon = GetSystemMetrics(SM_CYICON);
-		CRect rect;
-		GetClientRect(&rect);
-		int x = (rect.Width() - cxIcon + 1) / 2;
-		int y = (rect.Height() - cyIcon + 1) / 2;
+        // クライアントの四角形領域内の中央
+        int cxIcon = GetSystemMetrics(SM_CXICON);
+        int cyIcon = GetSystemMetrics(SM_CYICON);
+        CRect rect;
+        GetClientRect(&rect);
+        int x = (rect.Width() - cxIcon + 1) / 2;
+        int y = (rect.Height() - cyIcon + 1) / 2;
 
-		// アイコンの描画
-		dc.DrawIcon(x, y, m_hIcon);
-	}
-	else
-	{
-		CDialogEx::OnPaint();
-	}
+        // アイコンの描画
+        dc.DrawIcon(x, y, m_hIcon);
+    }
+    else
+    {
+        CDialogEx::OnPaint();
+    }
 }
 
 // ユーザーが最小化したウィンドウをドラッグしているときに表示するカーソルを取得するために、
 //  システムがこの関数を呼び出します。
 HCURSOR CMFC_RhythmGameDlg::OnQueryDragIcon()
 {
-	return static_cast<HCURSOR>(m_hIcon);
+    return static_cast<HCURSOR>(m_hIcon);
 }
 
 // --------------------------------
@@ -210,7 +212,8 @@ void CMFC_RhythmGameDlg::OnBnClickedButton2()
     tempoCount = 1.0;
 
     // スレッド制御
-    if (Metronome.get_id() == std::thread::id()) {
+    if (Metronome.get_id() == std::thread::id())
+    {
         // メトロノームを鳴らすスレッドを走らせる
         Metronome = std::thread(PlayMetronome);
     }
